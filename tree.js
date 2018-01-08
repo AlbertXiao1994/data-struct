35e0d08b4c0c4f1e76a86399867d5afa4638e8ca
Administrator@XTM-01701112049 <502583304@qq.com>
2018-01-08 19:49:43 +0800

    新增了多叉树的实现

--- tree.js
+++ tree.js
@@ -39,6 +39,53 @@ Tree.prototype.contains = function(callback, traverse) {
   traverse.call(this, callback)
 }
 
+Tree.prototype.add = function(data, toData, traverse) {
+  var child = new Node(data),
+      parent = null,
+      callback = function(node) {
+        if (node.data === toData) {
+          parent = node
+        }
+      }
+  this.contains(callback, traverse)
+  if (parent) {
+    parent.children.push(child)
+    child.parent = parent
+  } else {
+    throw new Error('Cannot add node to a non-existent parent')
+  }
+}
+
+Tree.prototype.remove = function(data, fromData, traverse) {
+  var parent = null,
+      callback = function(node) {
+        if (node.data === toData) {
+          parent = node
+        }
+      }
+  this.contains(callback, traverse)
+  if (parent) {
+    var index = findIndex(parent.children, data)
+    if (index !== undefined) {
+      parent.children.splice(index, 1)
+    } else {
+      throw new Error('Cannot delete a non-existent node')
+    }
+  } else {
+    throw new Error('Cannot delete node to a non-existent parent')
+  }
+}
+
+function findIndex(arr, data) {
+  var index
+  arr.forEach(function(x,i) {
+    if (x === data) {
+      index = i
+    }
+  })
+  return index
+}
+
 // 队列
 function Queue() {
   this._storage = {}
